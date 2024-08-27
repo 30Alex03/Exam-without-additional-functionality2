972f7b81fb114264f7609530420434f8ba28bc02
30Alex03 <163390461+30Alex03@users.noreply.github.com>
2024-08-27 17:24:50 +0300

    without additional functionality

---
+++ src/main/java/com/mycompany/examwork/ExamWork.java
@@ -0,0 +1,13 @@
+package com.mycompany.examwork;
+
+/**
+ *
+ * @author ivahn
+ */
+public class ExamWork {
+
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        gui.setVisible(true);
+    }
+}
